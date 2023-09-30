3dd68a7ac5d5602a4a3177ae49791e07944aa436
Ghouse Mohammed Shaik <ghousemohammed.shaik@gmail.com>
2023-10-01 00:31:47 +0530

    updated all packages to latest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
